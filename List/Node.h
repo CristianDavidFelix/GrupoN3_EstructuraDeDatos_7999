052b65c3a6266ea05270e339fd93ed36a610a7f5
SalazarMatthew <emsalazar1@espe.edu.ec>
2022-12-21 05:06:52 -0500

    busqueda

--- List/Node.h
+++ List/Node.h
@@ -1,3 +1,16 @@
+	/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+			DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+		    INGENIERA DE SOFTWARE
+		                
+			AUTORES:   Sebastian Paucar 
+					   Matthew Salazar
+					   Danilo Widinson
+					   Sebastian Solano 
+					   Cristian Felix 
+			FECHA DE CREACION:        20-12-2022 	
+			FECHA DE MODIFICACION:    21-12-2022*/
+
+
 #pragma once
 #include <iostream>
 
