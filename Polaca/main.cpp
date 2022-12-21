1bdf3fe9f5e1e1c4d54b6618d3b4986a0093ca66
CristianDavidFelix <david.df.01@hotmail.com>
2022-12-21 05:16:08 -0500

    Upgrade Folders

--- Polaca/main.cpp
+++ Polaca/main.cpp
@@ -1,10 +1,14 @@
-/*
-=======================================================
-UNIVERSIDAD DE LAS FUERZAS ARMADAS - "ESPE"
-=======================================================
-Autor:	Widincon Caiza
-=======================================================
-*/
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+            DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+            INGENIERA DE SOFTWARE
+
+            AUTORES:   Sebastian Paucar
+                       Matthew Salazar
+                       Danilo Widinson
+                       Sebastian Solano
+                       Cristian Felix
+            FECHA DE CREACION:        20-12-2022
+            FECHA DE MODIFICACION:    21-12-2022*/
 #include <iostream>
 #include <string>
 #include <sstream>
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GrupoN3_EstructuraDeDatos_7999
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+			DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+		    INGENIERA DE SOFTWARE
+		                
+			AUTORES:   Sebastian Paucar 
+					   Matthew Salazar
+					   Danilo Widinson
+					   Sebastian Solano 
+					   Cristian Felix 
+			FECHA DE CREACION:        20-12-2022 	
+			FECHA DE MODIFICACION:    21-12-2022*/
