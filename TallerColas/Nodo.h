a0930e5db797059b32b5e9861802ac7ac50375ce
CristianDavidFelix <david.df.01@hotmail.com>
2022-12-21 05:11:38 -0500

    Actualizacion de .h

--- TallerColas/Nodo.h
+++ TallerColas/Nodo.h
@@ -1,12 +1,18 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+			DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+			INGENIERA DE SOFTWARE
+
+			AUTORES:   Sebastian Paucar
+					   Matthew Salazar
+					   Danilo Widinson
+					   Sebastian Solano
+					   Cristian Felix
+			FECHA DE CREACION:        20-12-2022
+			FECHA DE MODIFICACION:    21-12-2022*/
+
 #pragma once
 #include <cstddef>
 
-/*Universidad de las Fuerzas Armadas "ESPE"
-Software
-Autores : Cantuna Michelle, Medina Martin, Perez Hamilton, Romero Jorge y Valarezo Andres
-Taller
-Fecha creacion : 13 / 06 / 2021
-Fecha de modificacion : 21 / 06 / 2021 */
 
 #if !defined(__UML_Class_Diagram_2_Nodo_h)
 #define __UML_Class_Diagram_2_Nodo_h
--- TallerColas/Tail.h
+++ TallerColas/Tail.h
@@ -1,3 +1,15 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+			DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+			INGENIERA DE SOFTWARE
+
+			AUTORES:   Sebastian Paucar
+					   Matthew Salazar
+					   Danilo Widinson
+					   Sebastian Solano
+					   Cristian Felix
+			FECHA DE CREACION:        20-12-2022
+			FECHA DE MODIFICACION:    21-12-2022*/
+
 #pragma once
 #if !defined(__UML_Class_Diagram_2_List_h)//CAMBIAR ESO
 #define __UML_Class_Diagram_2_List_h  ///CAMBIAR ESO
